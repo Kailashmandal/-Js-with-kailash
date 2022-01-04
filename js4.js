a6a18379e91ef71768777fcf1046bc302473c3c7
kailashmandal <hashtagkailash@gmail.com>
2022-01-04 17:38:34 +0530

    added some basics

---
+++ js4.js
@@ -0,0 +1,38 @@
+// *L-5 Declaring variables and Data types in js*
+
+// JavaScript variables-
+// Those elements which are used to store data values in programs.
+
+// JavaScript uses the keywords *var, let and const* to declare variables.
+
+// An equal sign is used to assign values to variables.
+
+// In this example, x is defined as a variable. Then, x is assigned (given) the value 6:
+
+// let x;
+// x = 6;
+
+// Data types : data types are used in context of  a variable it's important know that element is of which data type so that we can perform operations on it.
+
+// Some data types are : number , string , Boolean , undefined ,Symbols , Object , arrays etc.
+
+// *you don't have much worry about arrays and objects we will see them in details later*
+// for better understanding you can watch these videos -
+
+// 1. https://youtu.be/808eYu9B9Yw
+
+// 2. https://youtu.be/ycLqC41TO1U
+
+// *Ask doubts if you have any*
+
+var firstNum=66; // decalaring var type variable 
+var secondNum=77; // these can modified
+
+/** decalaring with let keyword */
+let percentage = 99;
+percentage=74; // these can also be modified 
+
+const myname="kailash";
+myname="mandal"; // these can not be modified
+
+console.log(myname); // will give an error
